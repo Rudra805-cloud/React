1eda6bbf5d91db0d5f3e5f018bee9cc478aa2c41
Rudra805 <rudramishra9949@gmail.com>
2025-06-08 13:07:11 +0530

    mega project authenticaton service

---
+++ 12-BlogsProject/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {import.meta.env.VITE_APPWRITE_URL
+console.log()
+
+
+  return (
+    <>
+      <h1>Blog app in appwrite</h1>
+    </>
+  )
+}
+
+export default App
